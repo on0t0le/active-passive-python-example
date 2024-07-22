61668b96ffcf48b22832cd0603c2e366b9090408
anatolii.b <anatolii.b@opsworks.co>
2024-07-22 22:51:05 +0300

    Added files to test locally

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import time
 import redis
 import os
 import uuid
+import threading
+import pika
 
+# Fetch configuration from environment variables
 INSTANCE_ID = str(uuid.uuid4())
 ROLE = 'standby'  # Initial role
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
+RABBITMQ_PORT = os.getenv('RABBITMQ_HOST', '5672')
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
+
+connection = None
+channel = None
 
 def send_heartbeat():
-    r = redis.StrictRedis(host='localhost', port=6379)
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
     while True:
         if ROLE == 'active':
             r.set('heartbeat', 'active', ex=10)
@@ -15,7 +25,7 @@ def send_heartbeat():
 
 def check_heartbeat():
     global ROLE
-    r = redis.StrictRedis(host='localhost', port=6379)
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
     while True:
         heartbeat = r.get('heartbeat')
         if heartbeat is None and ROLE == 'standby':
@@ -23,12 +33,11 @@ def check_heartbeat():
             if try_to_become_leader(r):
                 print(f"Instance {INSTANCE_ID}: Promoted to active.")
                 ROLE = 'active'
-                # Start the Flash application service or perform any necessary promotion actions
-                os.system('systemctl start flash_application.service')  # Example command
+                connect_to_rabbitmq()
         elif heartbeat is not None and ROLE == 'active':
             print(f"Instance {INSTANCE_ID}: Detected another active instance. Switching to standby.")
             ROLE = 'standby'
-            os.system('systemctl stop flash_application.service')  # Example command to stop service
+            disconnect_from_rabbitmq()
         time.sleep(5)
 
 def try_to_become_leader(redis_client):
@@ -38,9 +47,25 @@ def try_to_become_leader(redis_client):
         return True
     return False
 
+def connect_to_rabbitmq():
+    global connection, channel
+    credentials = pika.PlainCredentials('guest', 'guest')
+    parameters = pika.ConnectionParameters(RABBITMQ_HOST,
+                                        RABBITMQ_PORT,
+                                        '/',
+                                        credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    print(f"Instance {INSTANCE_ID}: Connected to RabbitMQ")
+
+def disconnect_from_rabbitmq():
+    global connection, channel
+    if connection:
+        connection.close()
+    print(f"Instance {INSTANCE_ID}: Disconnected from RabbitMQ")
+
 if __name__ == "__main__":
     # Run both heartbeat and failover checks
-    import threading
     heartbeat_thread = threading.Thread(target=send_heartbeat)
     heartbeat_thread.start()
     check_heartbeat()
